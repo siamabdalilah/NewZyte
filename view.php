1ddd13b200285583352cd44bd57935d6183849ae
Siam Abd Al-Ilah <siamabdal-ilah@Siams-MacBook-Pro.local>
2018-09-23 14:37:47 -0500

    corr

--- view.php
+++ view.php
@@ -4,7 +4,7 @@
 	//if (isset$_SESSION['user'])
 	$id = isset($_GET['id'])? $_GET['id'] : null;
 	$usr = isset($_SESSION['user'])? $_SESSION['user'] : null;
-	$stmt = $sqli->prepare('select title, stories, owner from tables where id = ?');
+	$stmt = $sqli->prepare("select title, stories, owner from tables where id = ?");
 	$stmt->bind_param('s', $id);
 	$stmt->execute();
 	$stmt->bind_result($title, $story, $owner);
@@ -24,7 +24,7 @@
 			if ($stmt->fetch()){
 				echo "<h3> $title </h3> <br>Written by $owner<br><p>$story</p>";
 				$comments = $sqli->preapre("select owner, comment from comments where story = ?");
-				$comments->bind_params('s', $id);
+				$comments->bind_param('s', $id);
 				$comments->execute();
 				$comments->bind_result($user, $comment);
 
