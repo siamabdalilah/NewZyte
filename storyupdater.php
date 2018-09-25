8c2317f48a271073a8af0bb5ab63074db893cab3
Siam Abd Al-Ilah <siamabdal-ilah@siams-mbp.dhcp.wustl.edu>
2018-09-25 16:48:26 -0500

    story updater should work in theory

---
+++ storyupdater.php
@@ -0,0 +1,24 @@
+<?php
+	session_start();
+	require 'database.php';
+
+	if (!isset($_SESSION['user']) || !isset($_POST['title'])){
+		header("Location: index.php");
+		exit;
+	}
+
+	$story = $_POST['story'];
+	$title = $_POST['title'];
+	$id = $_POST['id'];
+	$user = $_SESSION['user'];
+
+	$stmt = $sqli->prepare('update stories (stories, title) values (?, ?) where id = ?');
+	$stmt->bind_param('sss', $story, $title, $id);
+	$stmt->execute();
+
+	
+	$stmt->close();
+
+	header("Location: index.php");
+	exit;
+?>
