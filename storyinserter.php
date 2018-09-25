497f4d8212d92bf07d9481d53de0b766bfed7200
Siam Abd Al-Ilah <siamabdal-ilah@siams-mbp.dhcp.wustl.edu>
2018-09-24 20:29:01 -0500

    Should be able to insert stories

---
+++ storyinserter.php
@@ -0,0 +1,33 @@
+<?php
+	session_start();
+	require 'database.php';
+
+	if (!isset($_SESSION['user']) || !isset($_POST['title'])){
+		header("Location: index.php");
+		exit;
+	}
+
+	$story = $_POST['story'];
+	$title = $_POST['title'];
+
+	$user = $_SESSION['user'];
+
+	$stmt = $sqli->prepare('insert into stories (owner, sotries, title) values ( ?, ?, ?)');
+	$stmt-bind_param('sss', $user, $story, title);
+	$stmt->execute();
+
+	$quer = $sqli->prepare('select id from strories where title = ?');
+	$quer->bind_param('s', $title);
+	$quer->execute();
+	$quer->bind_result($id);
+
+	$quer->fetch();
+	$link = 'ec2-52-15-37-3.us-east-2.compute.amazonaws.com/~siamabdalilah/newssite/view?id='.$id;
+
+	$quer2->prepare('update stories set link = ? where id = ?');
+	$quer2->bind_param('ss', $link, $id);
+	$quer2->execute();
+
+	header("Location: index.php");
+	exit;
+?>
